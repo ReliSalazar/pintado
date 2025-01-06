6fa6f58d84fc331e3397f031220cf16346aa1645
Reli <naitvenefy@gmail.com>
2025-01-05 19:16:24 -0600

    home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import FullScreenMenu from "./components/FullScreenMenu";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 
-const menuOptions = ["Home", "About", "Services", "Contact"];
-
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => setMenuOpen(!menuOpen);
-
   return (
     <div className="relative h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth">
-      <Navbar menuOpen={menuOpen} toggleMenu={toggleMenu} />
-
-      <FullScreenMenu isOpen={menuOpen} menuOptions={menuOptions} />
-
       <div className="snap-start">
         <Home />
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import FullPageSection from "../components/FullPageSection";
+import { Mail, Smartphone } from "lucide-react";
 
 const Home = () => {
   return (
-    <FullPageSection id="home" className="bg-blue-500 text-white">
-      <div className="container border border-black">
-        <h1 className="text-5xl font-bold">Home Section</h1>
+    <FullPageSection id="home">
+      <div className="container flex flex-col items-center gap-20 px-4">
+        <div className="max-w-3xl flex flex-col md:flex-row gap-2 items-center">
+          <img alt="" src="/icon.png" className="max-w-32 md:max-w-40" />
+          <img alt="" src="/text.png" className="w-9/12" />
+        </div>
+
+        <div className="container flex flex-col items-center gap-2">
+          <div className="flex gap-4 text-zinc-300 hover:text-white ease-in-out duration-300">
+            <Smartphone />
+            <a href="tel:7721244386">772 124 4386</a>
+          </div>
+
+          <div className="flex gap-4 text-zinc-300 hover:text-white ease-in-out duration-300">
+            <Mail />
+            <a href="mailto:email@example.com">email@example.com</a>
+          </div>
+        </div>
       </div>
     </FullPageSection>
   );
